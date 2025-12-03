ea8edfb7028ee662a18ee4c1eba75833d9bbc169
Nguyen Ngo <24ngont@gmail.com>
2025-12-03 13:55:06 -0500

    Issue #7 fixed: Remove the unnecessary R data wrangling file
    Only need QMD file

--- worldHappinessReportDataWrangling.R
+++
@@ -1,7 +0,0 @@
-library(dplyr)
-
-#import dataset and filter it to only display records for 2024
-worldHappinessIndex <- read_excel("World Happiness Report Data Figure 2.1.xlsx") %>%
-  filter(Year == 2024) %>%
-  arrange(Rank)
-
